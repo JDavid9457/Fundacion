35928cbf81cdb34b3edd7b88c0e7039a8e681de5
JDavid <jdsanchez872@gmail.com>
2019-04-08 14:51:52 -0500

    c2

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')+h%icfhfjax*+ouqq(dsixdbj@!^0(^(4w&*dw-x8wa4z6fkh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
